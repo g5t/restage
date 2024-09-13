9998c188871abebe8681578db430c618c624e970
Gregory Tucker <gregory.tucker@ess.eu>
2024-09-13 20:38:23 +0200

    [Add] hacked-in MCPL_input_once.comp registry available for all loaded Instr files

--- src/restage/instr.py
+++ src/restage/instr.py
@@ -4,11 +4,27 @@ Utilities for interfacing with mccode_antlr.instr.Instr objects
 from pathlib import Path
 from typing import Union
 from mccode_antlr.instr import Instr
+from mccode_antlr.reader import Registry
 
 
-def load_instr(filepath: Union[str, Path]) -> Instr:
+def special_load_mcstas_instr(filepath: Path, extra_registries: list[Registry] | None = None):
+    from antlr4 import CommonTokenStream, InputStream
+    from mccode_antlr.grammar import McInstrParser, McInstrLexer
+    from mccode_antlr.instr import InstrVisitor
+    from mccode_antlr.reader import Reader, MCSTAS_REGISTRY
+    contents = filepath.read_text()
+    parser = McInstrParser(CommonTokenStream(McInstrLexer(InputStream(contents))))
+    registries = [MCSTAS_REGISTRY] if extra_registries is None else [MCSTAS_REGISTRY] + extra_registries
+    reader = Reader(registries=registries)
+    visitor = InstrVisitor(reader, str(filepath))
+    instr = visitor.visitProg(parser.prog())
+    instr.flags = tuple(reader.c_flags)
+    instr.registries = tuple(registries)
+    return instr
+
+
+def load_instr(filepath: Union[str, Path], extra_registries: list[Registry] | None = None) -> Instr:
     """Loads an Instr object from a .instr file or a HDF5 file"""
-    from mccode_antlr.loader import load_mcstas_instr
     from mccode_antlr.io import load_hdf5
 
     if not isinstance(filepath, Path):
@@ -16,10 +32,23 @@ def load_instr(filepath: Union[str, Path]) -> Instr:
     if not filepath.exists() or not filepath.is_file():
         raise ValueError('The provided filepath does not exist or is not a file')
 
-    if filepath.suffix == '.instr':
-        return load_mcstas_instr(filepath)
+    # FIXME this hack should be removed ASAP
+    if extra_registries is None:
+        from mccode_antlr.reader import GitHubRegistry
+        mcpl_input_once_registry = GitHubRegistry(
+            name='mcpl_input_once',
+            url='https://github.com/g5t/mccode-mcpl-input-once',
+            version='main',
+            filename='pooch-registry.txt'
+        )
+        extra_registries = [mcpl_input_once_registry]
 
-    return load_hdf5(filepath)
+    if filepath.suffix == '.instr':
+        return special_load_mcstas_instr(filepath, extra_registries=extra_registries)
+
+    instr = load_hdf5(filepath)
+    instr.registries += tuple(extra_registries)
+    return instr
 
 
 def collect_parameter_dict(instr: Instr, kwargs: dict, strict: bool = True) -> dict:
