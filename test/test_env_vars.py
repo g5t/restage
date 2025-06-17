b56305dc92205c7de78a0215dacc681f6da866ba
Gregory Tucker <gregory.tucker@ess.eu>
2025-06-17 08:27:24 +0200

    [Fix] Windows confuse.as_path adds drive letter -- only compare strings

--- test/test_env_vars.py
+++ test/test_env_vars.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 from unittest.mock import patch
-from pathlib import Path
 from importlib import reload
 import restage.config
 
@@ -12,14 +11,14 @@ class SettingsTests(TestCase):
         reload(restage.config)
         from restage.config import config
         self.assertTrue(config['cache'].exists())
-        self.assertEqual(config['cache'].as_path(), Path('/tmp/some/location'))
+        self.assertEqual(config['cache'].as_str(), '/tmp/some/location')
 
     @patch.dict(os.environ, {"RESTAGE_FIXED": "/tmp/some/location"})
     def test_restage_single_fixed_config(self):
         reload(restage.config)
         from restage.config import config
         self.assertTrue(config['fixed'].exists())
-        self.assertEqual(config['fixed'].as_path(), Path('/tmp/some/location'))
+        self.assertEqual(config['fixed'].as_str(), '/tmp/some/location')
 
     @patch.dict(os.environ, {'RESTAGE_FIXED': '/tmp/a /tmp/b /tmp/c'})
     def test_restage_multi_fixed_config(self):
@@ -28,16 +27,16 @@ class SettingsTests(TestCase):
         self.assertTrue(config['fixed'].exists())
         more = config['fixed'].as_str_seq()
         self.assertEqual(len(more), 3)
-        self.assertEqual(Path(more[0]), Path('/tmp/a'))
-        self.assertEqual(Path(more[1]), Path('/tmp/b'))
-        self.assertEqual(Path(more[2]), Path('/tmp/c'))
+        self.assertEqual(more[0],'/tmp/a')
+        self.assertEqual(more[1],'/tmp/b')
+        self.assertEqual(more[2],'/tmp/c')
 
     def test_restage_standard_config(self):
         from os import environ
         reload(restage.config)
         from restage.config import config
         if 'cache' in config:
-            self.assertEqual(config['cache'].as_path(), Path(environ['RESTAGE_CACHE']))
+            self.assertEqual(config['cache'].as_str(), environ['RESTAGE_CACHE'])
         if 'fixed' in config:
             self.assertEqual(config['fixed'].as_str(), environ['RESTAGE_FIXED'])
 
